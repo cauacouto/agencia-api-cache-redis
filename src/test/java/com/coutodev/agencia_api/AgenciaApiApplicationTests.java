70b6355ebfb7dde031238a96a0c672d3c9c987fb
cauacouto <cauacouto022@gmail.com>
2026-08-19 23:10:42 -0300

    add unit test for Agencia API application context

---
+++ src/test/java/com/coutodev/agencia_api/AgenciaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coutodev.agencia_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
